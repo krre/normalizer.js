29fc73b45af14b1cdfd4099c3c3babc0dbcf0e2e
Vladimir Zarypov <krre31@gmail.com>
2024-05-15 08:08:27 +0300

    Add request for WebGPU support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,41 @@
-<h1>Welcome to Normalizer</h1>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const title = 'Normalizer';
+
+	let device: string;
+	let error: string;
+
+	onMount(() => {
+		gpuInit();
+	});
+
+	async function gpuInit() {
+		if (!navigator.gpu) {
+			error = 'WebGPU not supported';
+			return;
+		}
+
+		const adapter = await navigator.gpu.requestAdapter();
+
+		if (!adapter) {
+			error = "Couldn't request WebGPU adapter";
+			return;
+		}
+
+		device = await adapter.requestDevice();
+
+		if (!device) {
+			error = "Couldn't request WebGPU device";
+			return;
+		}
+	}
+</script>
+
+<h1>{title}</h1>
+
+{#if error}
+	<p>Error: {error}</p>
+{:else}
+	{device}
+{/if}
