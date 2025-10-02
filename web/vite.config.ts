42025cd983eaf30cdb36979207f2188b6b4b5cee
Vladimir Zarypov <krre31@gmail.com>
2025-10-02 17:19:07 +0300

    Add ful page reload on update Wasm module

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,20 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		{
+			name: 'wasm-reload',
+			handleHotUpdate({ file, server }) {
+				if (file.endsWith('.wasm')) {
+					server.ws.send({
+						type: 'full-reload',
+						path: '*'
+					})
+				}
+			}
+		}
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
